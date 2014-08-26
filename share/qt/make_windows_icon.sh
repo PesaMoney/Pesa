733c4f5330365d99fcdcd1382dcd8b8df1c56513
Maxmillion McLaughlin <maxmillionmclaughlin@gmail.com>
2014-08-26 15:41:43 -0400

    Cleared out repository
    Will update with new wallet

--- share/qt/make_windows_icon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/pesa.ico
-
-convert ../../src/qt/res/icons/pesa-16.png ../../src/qt/res/icons/pesa-32.png ../../src/qt/res/icons/pesa-48.png ${ICON_DST}
